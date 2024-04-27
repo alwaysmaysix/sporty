0993006ad3a9daffdcfac02cbd70d6d727001fca
alwaysmaysix <104639059+alwaysmaysix@users.noreply.github.com>
2024-04-28 01:58:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ current_result = ""
 
 # set environment variables
 # bot api
-api_key = ""
+api_key = "6747030829:AAEZtSPE-ZOWWGv0E2p2KPLNM5ztpOrCkX8"
 
 # us sports news
 ussports = ""
 
 # uk sports news
-uksports = ""
+uksports = "fac94debf7f7442d9a297ca0ea4ba6fd"
